3536b987ad499ab48945813559f879d7521ecd08
Mohit <mohitkg2601@gmail.com>
2024-01-25 13:30:08 +0530

    edit,delete,date added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,37 +1,87 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaEdit } from "react-icons/fa";
 import { RiDeleteBin3Line } from "react-icons/ri";
 
+
+//getting local data
+const getLocalData=(()=>{
+    const itemslist=localStorage.getItem("myList")
+    if(itemslist){
+        return JSON.parse(itemslist);
+    }else{
+        return [];
+    }
+})
+
 const Home = () => {
-const [inputData,SetInput]=useState("");
-const [list,setList]=useState([]);
+const [inputData,setInput]=useState("");
+const [list,setList]=useState(getLocalData());
+const [isEdited,setEdited]=useState("");
+const [toggleBtn,settoggleBtn]=useState(false);
+const [inputDate,setInputDate]=useState(new Date());
 
 const changeHandler=((e)=>{
-    SetInput(e.target.value);
+    setInput(e.target.value);
+   
 });
 
 //adding 
 const addItem=(()=>{
-
+    if(!inputData){
+        alert("please enter something");
+    }
+    else if(inputData && toggleBtn){
+        setList(
+            list.map((ele)=>{
+                console.log(isEdited);
+                if(ele.id===isEdited){
+                    return {...ele,data:inputData};
+                }
+                return ele;
+            })
+        )
+        setInput("");
+        setEdited(null);
+        settoggleBtn(false);
+    }
+    else{
     const itemList={
         id: Math.random(),
         data: inputData,
+        date:inputDate,
     }
-    setList([list,itemList]);
-    SetInput("");
-    console.log(list);
+    setList([...list,itemList]);
+    setInput("");
+    // console.log(list);
     
-    
+}
 })
 
 //Deleting 
-const delteItem=((id)=>{
-        console.log(id);
-        // const newList=list.filter((item)=> item.id!==id);
-        // setList(newList);
-})
+
+const deleteItem=((idx)=>{
+        // // console.log(id);
+        const newList=list.filter((ele)=>{
+            return ele.id!==idx});
+        setList(newList);
+        // console.log(newList);
+});
 //editing 
-    const editItem=(()=>{});
+    const editItem=((idx)=>{
+        // // console.log(id);
+        const editing=list.find((ele)=> {
+        return ele.id===idx});
+        setInput(editing.data);
+        setInputDate(editing.date);
+        setEdited(idx);
+        settoggleBtn(true);
+    });
+
+
+//Adding local storage
+useEffect(()=>{
+           localStorage.setItem("myList",JSON.stringify(list));   
+},[list]);
 
 
 
@@ -39,17 +89,26 @@ const delteItem=((id)=>{
     <div className="main">
       <h1>Task Tracker</h1>  
         <div className="items">
+            <div className="inputs">
             <input type="text" placeholder="Add task here..." value={inputData} onChange={changeHandler}/>
-            <button onClick={addItem}>Add</button>
+            <input type="date"  value={inputData} onChange={(e)=> setInputDate(e.target.value)}/>
+            </div>
+            { toggleBtn ? (<button onClick={addItem}>Add </button>):(
+                <button onClick={addItem}>Add New</button>
+            )}
         </div>
         <div className="itemlist">
            <ul>
-            {list.map((currenItem) =>{
+            {list.map((curItem) =>{
                 return(
-                <li key={currenItem.id}>
-                    {currenItem.data}
-                    <div className="edit"  onClick={editItem(currenItem.id)}><FaEdit/></div>
-                    <div className="delete" onClick={delteItem(currenItem.id)}><RiDeleteBin3Line/></div> 
+                <li key={curItem.id}>
+                   <div> {curItem.data}
+                   {curItem.date}</div>
+              <div className="task-btn">
+                <div onClick={()=>deleteItem(curItem.id)}><RiDeleteBin3Line/></div>
+                <div onClick={()=>editItem(curItem.id)}><FaEdit/></div>
+              </div>
+                    
                 </li>
            ) })}
            </ul>   
